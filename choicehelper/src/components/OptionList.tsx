2299309f62b9787b011d0d3d7f05b02bcaf88b04
ksa37 <ksa37@kaist.ac.kr>
2022-02-02 18:49:26 +0900

    Update textarea width

--- choicehelper/src/components/OptionList.tsx
+++ choicehelper/src/components/OptionList.tsx
@@ -4,10 +4,10 @@ import '../App.css';
 
 
 
-const defaultColor = [0, 1];
+const defaultColor = [0, 1, 2];
 const getOptionClouds = () =>{
   let content = [];
-  for(let i=0; i<2; i++){
+  for(let i=0; i<3; i++){
     content.push(<OptionCloud color={defaultColor[i]} key={i} />);
   }
   return content;
