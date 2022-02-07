b2dbb32e1c2c844430c2b3b1802c76010ecc1bce
Jiyun Park <wldbs0507@kaist.ac.kr>
2022-02-07 18:39:48 +0900

    Connect mongodb with mongoose

--- server/index.js
+++ server/index.js
@@ -1,30 +1,51 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+// const express = require('express');
 const app = express();
 
-const http = require('http').createServer(app);
-http.listen(5000, function () {
-    console.log('listening on 5000')
-});
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors())
 
-router.post("/selection", (req, res) => {
-    const { options, timestamp, repeat, selected_option } = req.body
-    console.log(req.body)
-    // db에 넣어주기
-});
+// Due to security issue, move this variable to .env later
+const CONNECTION_URL = 'mongodb+srv://choicehelper:choicehelper123@cluster0.cb2bq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 4000;
 
-router.get("/recent-topics", (req, res) => {
-    console.log("최근 고민")
-    res.send("최근 고민들 db에서 json 형태로")
-});
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message));
 
-router.get("/hot-topics", (req, res) => {
-    console.log("핫한 고민")
-    res.send("핫한 고민들 db에서 json 형태로")
-});
+// Got error 
+// mongoose.set('useFindAndModify', false);
 
-router.get("/total", (req, res) => {
-    console.log("서비스 사용횟수")
-    res.send("서비스를 총 몇 번 사용했는지 횟수")
-});
+// const http = require('http').createServer(app);
+// http.listen(5000, function () {
+//     console.log('listening on 5000')
+// });
+
+// router.post("/selection", (req, res) => {
+//     const { options, timestamp, repeat, selected_option } = req.body
+//     console.log(req.body)
+//     // db에 넣어주기
+// });
+
+// router.get("/recent-topics", (req, res) => {
+//     console.log("최근 고민")
+//     res.send("최근 고민들 db에서 json 형태로")
+// });
+
+// router.get("/hot-topics", (req, res) => {
+//     console.log("핫한 고민")
+//     res.send("핫한 고민들 db에서 json 형태로")
+// });
+
+// router.get("/total", (req, res) => {
+//     console.log("서비스 사용횟수")
+//     res.send("서비스를 총 몇 번 사용했는지 횟수")
+// });
 
 // ts로 바꾸기
