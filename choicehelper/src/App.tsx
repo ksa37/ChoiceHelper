3788409f755fd13677efcbf364dff4b1d2301a09
Jiyun Park <wldbs0507@kaist.ac.kr>
2022-02-26 15:34:07 +0900

    Post test works without date format

--- choicehelper/src/App.tsx
+++ choicehelper/src/App.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import { createPost } from './api';
 import './App.css';
 import Button from './components/Button';
 import Header from './components/Header';
 import OptionCloud from './components/OptionCloud';
 import OptionList from './components/OptionList';
 
+function onClick() {
+  const options: string[] = [];
+  const selected_option: string = '';
+  const createdAt: Object = {};
+  createPost({
+    "options": options,
+    "selected_option": selected_option,
+    "createdAt": createdAt
+  })
+}
+
 function App() {
+  
+  // const data = createPost({
+  //   options: [],
+  //   selected_option: '',
+  //   createAt: {}
+  // })
+
   return (
     <>
       <Header/>
       <OptionList/>
-      <Button buttonOption={0}/>
+      <Button onClick={onClick} buttonOption={0}/>
     </>
   );
 }
--- choicehelper/src/components/Button.tsx
+++ choicehelper/src/components/Button.tsx
@@ -1,11 +1,13 @@
+import axios from 'axios';
 import React, { useState } from 'react';
+import { createPost } from '../api';
 import '../App.css';
 
 
 export default function Button({buttonOption}:any){
   const optionText = ["골라줘!", "다른 것도 골라줘!"]
   const [btnOpt, setBtnOpt] = useState(buttonOption);
-  const onClick = ()=>{
+  const onClick = async () => {
 
     // const picked = randomPick(options);
 
@@ -18,6 +20,53 @@ export default function Button({buttonOption}:any){
         break;
       }
     }
+
+    const options: string[] = ["hi"];
+    const selected_option: string = 'hello';
+    const repeat: number = 0;
+    const createdAt: Object = {};
+
+    // axios.post('http://localhost:4000/posts', {
+    //   "options": options,
+    //   "repeat": repeat,
+    //   "selected_option": selected_option
+    //   // "selected_option": selected_option,
+    //   // "createdAt": createdAt
+    // }).then(function (res) {
+    //   console.log(res);
+    // }).catch(function (err){
+    //   console.log(err);
+    // })
+
+    try {
+      const { data } = await createPost({
+        "options": options,
+        "repeat": repeat,
+        "selected_option": selected_option,
+        // "createdAt": createdAt
+      })
+    } catch(error) {
+      if (error instanceof Error){
+        console.log(error.message);
+      }
+    }
+
+    // axios.get('http://localhost:4000/posts')
+    //     .then((response) => {console.log(response)})
+    //     .catch((error) => {
+    //       if (error instanceof Error){
+    //         console.log(error.message);
+    //         }
+    //     })
+    
+    // axios({
+    //   method: 'get',
+    //   url: 'http://localhost:4000/posts',
+    //   responseType: 'stream'
+    // })
+    //   .then(function (response) {
+    //     console.log(response)
+    //   });
   }
 
   function randomPick(optionNumber: number){
