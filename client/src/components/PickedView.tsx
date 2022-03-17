17ac4763e2bd283619e82e73d6a5de2ea976a55f
ksa37 <ksa37@kaist.ac.kr>
2022-03-17 17:34:19 +0900

    Add going back function to second page

--- client/src/components/PickedView.tsx
+++ client/src/components/PickedView.tsx
@@ -3,6 +3,7 @@ import Button from './Button';
 import '../App.css';
 import OptionCloud from './OptionCloud';
 import PickedOptionList from './PickedOptionList';
+import { Link } from 'react-router-dom';
 
 export default function PickedView(){
   const backgroundColors = ['#FFF8E8', '#FFF2F2', '#F2F7FF'];
@@ -12,7 +13,9 @@ export default function PickedView(){
   
   return(
     <div style={{backgroundColor: backgroundColors[0], height: '100vh'}} >
-      <p style={{textAlign:'right', margin:'0 10px', paddingTop:'10px'}}>다른 것도 골라줘!</p>
+      <Link to='/' style={{ textDecoration: 'none', color:'black'}}>
+        <p style={{textAlign:'right', margin:'0 10px', paddingTop:'10px'}}>다른 것도 골라줘!</p>
+      </Link>
       <h2 style={{margin:'0 10px' }}>당신의 운명은 ..</h2>
       <OptionCloud color={1}/>
       <p style={{textAlign: 'center'}}>총 {accPicked}번 골라줬어요</p>
