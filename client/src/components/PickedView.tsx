acd71e3f8c42e05805a43f1d12a030b7cd89f226
Jiyun Park <wldbs0507@kaist.ac.kr>
2022-03-23 22:26:39 +0900

    Add total num of usage

--- client/src/components/PickedView.tsx
+++ client/src/components/PickedView.tsx
@@ -3,24 +3,58 @@ import Button from './Button';
 import '../App.css';
 import OptionCloud from './OptionCloud';
 import PickedOption from './PickedOption';
+import { fetchPosts } from '../api';
+import { useAsync } from 'react-async';
+
+async function getData() {
+  const { data } = await fetchPosts();
+  return data;
+}
+
 
 export default function PickedView(){
   const backgroundColors = ['#FFF8E8', '#FFF2F2', '#F2F7FF'];
   const accPicked = 86;
-  // get data from db (최신순 정렬, 총 횟수)
-  // maybe with redux?
-  
-  return(
-    <div style={{backgroundColor: backgroundColors[0], height: '100vh'}} >
-      <p style={{textAlign:'right', margin:'0 10px', paddingTop:'10px'}}>다른 것도 골라줘!</p>
-      <h2 style={{margin:'0 10px' }}>당신의 운명은 ..</h2>
-      <OptionCloud color={1}/>
-      <p style={{textAlign: 'center'}}>총 {accPicked}번 골라줬어요</p>
-      <h2 style={{margin:'0 10px' }}>현재 다른 사람들은 ..</h2>
-      <PickedOption text='껌' color={0} />
-      <PickedOption text='사탕' color={1} />
-      <span>중에 골라줘!</span>
-      <Button buttonOption={1}/>
-    </div>
-  )
+
+  const { data: data, error, isLoading } = useAsync({
+    promiseFn: getData,
+  });
+
+
+  console.log(data);
+  if (data)
+    return(
+      <div style={{backgroundColor: backgroundColors[0], height: '100vh'}} >
+        <p style={{textAlign:'right', margin:'0 10px', paddingTop:'10px'}}>다른 것도 골라줘!</p>
+        <h2 style={{margin:'0 10px' }}>당신의 운명은 ..</h2>
+        <OptionCloud color={1}/>
+        <p style={{textAlign: 'center'}}>총 {data.length}번 골라줬어요</p>
+        {/* {data}
+          ? <p style={{textAlign: 'center'}}>총 0번 골라줬어요</p> 
+          : <p style={{textAlign: 'center'}}>총 1번 골라줬어요</p>  */}
+          {/* : <p style={{textAlign: 'center'}}>총 {data.length}번 골라줬어요</p>  */}
+
+        <h2 style={{margin:'0 10px' }}>현재 다른 사람들은 ..</h2>
+        <PickedOption text='껌' color={0} />
+        <PickedOption text={data[0].options[0]} color={0} />
+        <PickedOption text='사탕' color={1} />
+        <span>중에 골라줘!</span>
+        <Button buttonOption={1}/>
+      </div>
+    )
+  else
+    return(
+      <div style={{backgroundColor: backgroundColors[0], height: '100vh'}} >
+        <p style={{textAlign:'right', margin:'0 10px', paddingTop:'10px'}}>다른 것도 골라줘!</p>
+        <h2 style={{margin:'0 10px' }}>당신의 운명은 ..</h2>
+        <OptionCloud color={1}/>
+        <p style={{textAlign: 'center'}}>총 0번 골라줬어요</p>
+        {/* <p style={{textAlign: 'center'}}>총 {data.length}번 골라줬어요</p> */}
+        <h2 style={{margin:'0 10px' }}>현재 다른 사람들은 ..</h2>
+        <PickedOption text='껌' color={0} />
+        <PickedOption text='사탕' color={1} />
+        <span>중에 골라줘!</span>
+        <Button buttonOption={1}/>
+      </div>
+    )
 }
