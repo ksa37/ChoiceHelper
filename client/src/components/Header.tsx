a5ba6df3589a8dbb9b1df5735df117890a5b5892
Jiyun Park <wldbs0507@kaist.ac.kr>
2022-03-26 20:33:29 +0900

    Add css of header in main page

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faPlus} from '@fortawesome/free-solid-svg-icons';
 import { useSelector, useDispatch, RootStateOrAny } from 'react-redux';
@@ -26,7 +27,10 @@ export default function Header(){
   return(
   <div className="header-group">
     골라줘!
-    <button onClick={addCloud}><FontAwesomeIcon className='header-icon' icon={faPlus} /></button>
+    <button className='add-button' onClick={addCloud}>
+      {/* <FontAwesomeIcon className='header-icon' icon={faPlus} /> */}
+      <p>+추가하기</p>
+    </button>
   </div>
   )
 }
