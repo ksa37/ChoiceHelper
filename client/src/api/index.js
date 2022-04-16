911f03bdfc24ed524d97c8070d64b3e9a09c0e66
Jiyun Park <wldbs0507@kaist.ac.kr>
2022-04-16 19:21:21 +0900

    Deploy done in netlify and heroku

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const url = 'http://localhost:4000/posts';
+// const url = 'http://localhost:4000/posts';
+const url = 'https://choice-helper-diuni.herokuapp.com/posts';
+
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
